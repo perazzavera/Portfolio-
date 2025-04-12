5af63df4845541fb0cb6877833b9999702e5a2ee
Vera Lucia Perazza <vperazza5@gmail.com>
2025-04-12 23:49:00 +0200

    corrigindo erro formulário

--- src/pages/Contato/index.jsx
+++ src/pages/Contato/index.jsx
@@ -21,10 +21,10 @@ export default function Contato() {
 
     try {
       await emailjs.sendForm(
-        process.env.REACT_APP_EMAILJS_SERVICE_ID,
-        process.env.REACT_APP_EMAILJS_TEMPLATE_ID,
+        import.meta.env.VITE_EMAILJS_SERVICE_ID,
+        import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
         formRef.current,
-        process.env.REACT_APP_EMAILJS_PUBLIC_KEY
+        import.meta.env.VITE_EMAILJS_PUBLIC_KEY
       );
 
       setSuccess(true);
